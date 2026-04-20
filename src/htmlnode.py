11983be0325dba2a1d781a5f20fc8021e40b3981
Lyebon <sebas25garcia@gmail.com>
2026-04-20 17:47:50 -0300

    HTMLNode Tree

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,5 +19,37 @@ class HTMLNode():
             result += word
         return result
 
+
     def __repr__(self):
         return (f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})")
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, value=None, children=children, props=props )
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Node must have a tag")
+        if self.children is None:
+            raise ValueError("Node must have a children")
+        children_html=""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}>{children_html}</{self.tag}>"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, children=None, props=props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+        
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf node must have a value")
+        if self.tag is None:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return (f"HTMLNode({self.tag}, {self.value}, {self.props})")
