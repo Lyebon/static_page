e2d2f05916d79c10c8b8eaa5fe809e22ee19e041
Lyebon <sebas25garcia@gmail.com>
2026-04-26 21:29:06 -0300

    splitting text is funtional

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -38,13 +38,94 @@ class TestTextNode(unittest.TestCase):
     
     def test_split_italic(self):
         node = TextNode("The text makes me sick _boldtext rules_ but some times needs to exist", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "_", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
         expected = [
             TextNode("The text makes me sick ", TextType.TEXT),
             TextNode("boldtext rules", TextType.ITALIC),
             TextNode(" but some times needs to exist", TextType.TEXT),
         ]
-        self.assertEqual(new_nodes, expected)  
+        self.assertEqual(new_nodes, expected)
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with an [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+    
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            ],
+            new_nodes,
+        )
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an [go to boot dev link](https://www.boot.dev) and another [second link to go](https://www.boot.dev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("go to boot dev link", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second link to go", TextType.LINK, "https://www.boot.dev"),
+            ],
+            new_nodes,
+        )
+    
+    def test_split_everything(self):
+        node = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(node)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            new_nodes,
+        )
+
+    def test_markdown_to_blocks(self):
+        md = """
+            This is **bolded** paragraph
+
+            This is another paragraph with _italic_ text and `code` here
+            This is the same paragraph on a new line
+
+            - This is a list
+            - with items
+            """
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
 if __name__ == "__main__":
     unittest.main()
--- src/textdelimiter.py
+++ src/textdelimiter.py
@@ -1,23 +1,110 @@
+import re
 from textnode import TextNode, TextType
-
+'''
+Split nodes delimiter:
+Recibe una lista de nodos, un delimitador para separar texto y el tipo de texto a separar
+Si el texto ya tiene un formato lo agrega a la lista
+si no divide el texto, revisa y agrega nuevos nodos con las propiedades correctas
+'''
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     nodes = []
     for node in old_nodes:
-        #Si el nodo es texto comun directamente se agrega a la nueva lista
         if node.text_type != TextType.TEXT:
             nodes.append(node)
         else:
-        #Se separa el texto del nodo por el delimitador
             parts = node.text.split(delimiter)
-            #Si la cantidad de partes es par se devuelve una Excepcion de que no esta cerrado el texto
             if len(parts)%2 == 0:
                 raise Exception(f"The delimiter {delimiter} is missing")
             for i, part in enumerate(parts):
-                #Si el nodo es par en la locacion de la lista se agrega un nodo nuevo de tipo texto al resultado final
                 if i%2 == 0:
-                    nodes.append(TextNode(part, TextType.TEXT))
+                    if part == "":
+                        continue
+                    else:
+                        nodes.append(TextNode(part, TextType.TEXT))
                 else:
-                #Si el nodo es impar en la locacion de la lista se agrega el nodo con el texto y tipo de texto que pertenece
                     nodes.append(TextNode(part, text_type))
-    #Se retorna la lista final de nodos delimitados
     return nodes
+
+'''
+Extraen la informacion de Links o Imagenes para generar el nodo de un texto plano.
+devuelven una lista vacia si no encuentra nada o una lista de tuplas
+'''
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
+     
+
+'''
+Recibe una lista de nodos y se fija por nodos con propiedades
+si el texto tiene una imagen o link y agregarlos como TextNode nuevos
+'''
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        image_extract = extract_markdown_images(node.text)
+        if not image_extract:
+            new_nodes.append(node)
+        else:
+            node_text = node.text
+            for part in image_extract:
+                image_alt, image_link = part
+                split_text = node_text.split(f"![{image_alt}]({image_link})", 1)
+                if split_text[0] != "":
+                    new_nodes.append(TextNode(split_text[0], TextType.TEXT))
+                new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+                node_text = split_text[1]
+            if node_text != "":
+                new_nodes.append(TextNode(node_text, TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        link_extract = extract_markdown_links(node.text)
+        if not link_extract:
+            new_nodes.append(node)
+        else:
+            node_text = node.text
+            for part in link_extract:
+                link_alt, url_link = part
+                split_text = node_text.split(f"[{link_alt}]({url_link})", 1)
+                if split_text[0] != "":
+                    new_nodes.append(TextNode(split_text[0], TextType.TEXT))
+                new_nodes.append(TextNode(link_alt, TextType.LINK, url_link))
+                node_text = split_text[1]
+            if node_text != "":
+                new_nodes.append(TextNode(node_text, TextType.TEXT))
+    return new_nodes
+
+'''
+Genera de un texto plano la separacion respectiva de las partes
+armando TextNode validos con la oracion
+'''
+def text_to_textnodes(text):
+    text_node = [TextNode(text, TextType.TEXT)]
+    node_split = split_nodes_delimiter(text_node, "**", TextType.BOLD)
+    node_split = split_nodes_delimiter(node_split, "_", TextType.ITALIC)
+    node_split = split_nodes_delimiter(node_split, "`", TextType.CODE)
+    node_split = split_nodes_image(node_split)
+    node_split = split_nodes_link(node_split)
+    return node_split
+
+
+def markdown_to_blocks(markdown):
+    result = []
+    split_markdown = markdown.split("\n\n")
+    for text in split_markdown:
+        if text != "":
+            result.append(text)
+
+    return result
