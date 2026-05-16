f42db909735ae6148cb96975ef086b6b29fbdba7
Lyebon <sebas25garcia@gmail.com>
2026-05-16 15:57:16 -0300

    generate page added again

--- src/gencontent.py
+++ src/gencontent.py
@@ -22,23 +22,26 @@ def write_file(path, template):
         f.write(template)
 
 
+def generate_page(content_path, template_path, public_path):
+    print(f"Generating page from {content_path} to {public_path} using {template_path}")
+    markdown = read_file(content_path)
+    template = read_file(template_path)
+    title = extract_title(markdown)
+    html_nodes = markdown_to_html_node(markdown)
+    title_template = template.replace("{{ Title }}", title)
+    content = html_nodes.to_html()
+    final_template = title_template.replace("{{ Content }}",content)
+    directory = os.path.dirname(public_path)
+    os.makedirs(directory, exist_ok=True)
+    write_file(public_path, final_template)
+
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     content_dir = os.listdir(dir_path_content)
     for content in content_dir:
         content_path = dir_path_content/content
         if os.path.isfile(content_path):
-            public_path = dest_dir_path/"index.html"
-            print(f"Generating page from {content_path} to {public_path} using {template_path}")
-            markdown = read_file(content_path)
-            template = read_file(template_path)
-            title = extract_title(markdown)
-            html_nodes = markdown_to_html_node(markdown)
-            title_template = template.replace("{{ Title }}", title)
-            content = html_nodes.to_html()
-            final_template = title_template.replace("{{ Content }}",content)
-            directory = os.path.dirname(public_path)
-            os.makedirs(directory, exist_ok=True)
-            write_file(public_path, final_template)
+            public_path = dest_dir_path/content
+            generate_page(content_path, template_path, public_path)
         else:
             public_path = dest_dir_path/content
             generate_pages_recursive(content_path, template_path, public_path)
