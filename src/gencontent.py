0bd88c09fb9e338bb70269c664c78f585ddb4ce0
Lyebon <167833071+Lyebon@users.noreply.github.com>
2026-05-18 18:52:29 -0300

    Add basepath parameter to page generation functions
    Updated functions to include basepath parameter for generating pages.

--- src/gencontent.py
+++ src/gencontent.py
@@ -22,7 +22,7 @@ def write_file(path: pathlib.Path, template: pathlib.Path) -> None:
         f.write(template)
 
 
-def generate_page(content_path: pathlib.Path, template_path: pathlib.Path, public_path: pathlib.Path) -> None:
+def generate_page(content_path: pathlib.Path, template_path: pathlib.Path, public_path: pathlib.Path, basepath) -> None:
     print(f"Generating page from {content_path} to {public_path} using {template_path}")
     markdown = read_file(content_path)
     template = read_file(template_path)
@@ -37,13 +37,13 @@ def generate_page(content_path: pathlib.Path, template_path: pathlib.Path, publi
     os.makedirs(directory, exist_ok=True)
     write_file(public_path, final_template)
 
-def generate_pages_recursive(dir_path_content: pathlib.Path, template_path: pathlib.Path, dest_dir_path: pathlib.Path) -> None:
+def generate_pages_recursive(dir_path_content: pathlib.Path, template_path: pathlib.Path, dest_dir_path: pathlib.Path, basepath) -> None:
     content_dir = os.listdir(dir_path_content)
     for content in content_dir:
         content_path = dir_path_content/content
         if os.path.isfile(content_path):
-            public_path = content
-            generate_page(content_path, template_path, public_path)
+            docs_path = dest_dir_path/content
+            generate_page(content_path, template_path, docs_path, basepath)
         else:
-            public_path = content
-            generate_pages_recursive(content_path, template_path, public_path)
+            docs_path = dest_dir_path/content
+            generate_pages_recursive(content_path, template_path, docsc_path, basepath)
